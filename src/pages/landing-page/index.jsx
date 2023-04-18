5d5ee6ec78ffcd64c3458f8fbe9b24289f17b36e
Mayur917 <chodankarmayur1@gmail.com>
2023-04-18 22:04:50 +0530

    Basics

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -1,37 +1,45 @@
 import React from 'react'
 import styles from './Landing.module.css'
-
+import CustomButton from '@/components/CustomButton/customButton'
+import logo from '@/assets/icons/logo.png'
+import Image from 'next/image'
 
 const mypage = () => {
     return (
         <>
-    <header>
-        <navbar>
-            <div className={styles.nav-left}>
-                <img src="./logo.png" alt="logo" style="height: 40px;"/>
-                <h3>
-                    <a href="#">KeyVision</a>
-                </h3>
-            </div>
-            <div className={styles.nav-right}>
-                <h4>
-                    <a href="#">About Us</a>
-                </h4>
-                <h4> 
-                    <a href="#">Need Help?</a>
-                </h4>
-                <h4>
-                    <a href="#">Login</a>
-                </h4>
-            </div>
-        </navbar>
-    </header>
-    <main>
+            <header>
+                <navbar className={styles.nav}>
+                    <div className={styles.navleft}>
+                        <div className={styles.logowrapper + " d-flex align-items-center justify-content-between"}>
+                            <div className='d-flex align-items-center' >
+                                <Image
+                                    src={logo}
+                                    alt="logo"
+                                    className={styles.logo}
+                                />
+                                <p className={styles.okr + ' ms-2'} >KeyVision</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.navright}>
+                        <h4>
+                            <a className={'link'} href="#">About Us</a>
+                        </h4>
+                        <h4>
+                            <a className={'link'} href="#">Need Help?</a>
+                        </h4>
+                        <h4>
+                            <CustomButton text={"Login"} onClick={() => submitForm()} className={styles.butn} />
+                        </h4>
+                    </div>
+                </navbar>
+            </header>
+            <main className={styles.maincontent}>
 
-    </main>
-    <footer>
+            </main>
+            <footer>
 
-    </footer>
+            </footer>
         </>
     )
 }
