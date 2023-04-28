18cb177ca280737fe316f0560e8886b54ee6afad
ranjana-r-rajeev <ranjanarajeev0123@gmail.com>
2023-04-28 20:53:43 +0530

    terms&conditions

--- src/pages/terms&conditions/index.jsx
+++ src/pages/terms&conditions/index.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import styles from "./terms&conditions.module.css"
 import logo from "@/assets/icons/logo.png";
 
 const TermsCondition = () => {
     return (
         <div>
-            hello world
+            <div className={styles.main}>
+                <div className={styles.smain}>
+                    <div  className={styles.title}><h1>Terms&Conditions</h1></div>
+                    Objectives and Key Results (OKRs) are a popular framework used to set and track goals in many organizations. While the specific terms and conditions for OKRs may vary depending on the organization, here are some general guidelines:<br/>
+
+                    1. Objectives should be specific, measurable, achievable, relevant, and time-bound (SMART).<br/>
+
+                    2. Key Results should be quantifiable and have a clear target that can be objectively measured.<br/>
+
+                    3. Objectives and Key Results should be aligned with the overall mission and vision of the organization.<br/>
+
+                    4. Objectives and Key Results should be reviewed regularly and adjusted as needed to ensure progress is being made. <br/>
+
+                    5. Confidential information should not be included in objectives or key results.<br/>
+
+                    6. Objectives and Key Results should be challenging but achievable, and employees should be provided with the necessary resources and support to meet them.<br/>
+
+                    7. Objectives and Key Results should be transparent and visible to all relevant stakeholders.<br/>
+
+                    8. Employees should be held accountable for meeting their objectives and key results, and performance should be evaluated based on progress made toward these goals.<br/>
+
+                    9. Objectives and Key Results should not be used as the sole determinant of compensation or promotion decisions.<br/>
+
+                    10. The organization may modify or terminate the use of OKRs at any time, at its sole discretion.<br/>
+
+                    It's important to note that these are general guidelines, and organizations may have specific terms and conditions for OKRs that align with their unique culture, values, and goals.
+                     
+                </div>
+            </div>
         </div>
         );
     };
