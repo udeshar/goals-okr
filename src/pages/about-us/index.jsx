57b95f3138d1946394c60e747ebc81877281b85c
ranjana-r-rajeev <ranjanarajeev0123@gmail.com>
2023-04-28 22:36:24 +0530

    about-us updates

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -7,8 +7,8 @@ const AboutUs = () => {
   
     <div className={styles.main}>
         <div className={styles.first}>
-            <div className={styles.second}>ABOUT US</div>
-        </div>
+            <div className={styles.second}><b>ABOUT-US</b><br /></div>
+            <br />
         <div className={styles.info}>
             <p>At Key Vision, we believe in setting clear objectives and key results to drive our organization towards success. <b>Our mission is to drive organizational success by setting clear objectives and key results. We believe that by aligning our goals and tracking progress, we can continuously improve and achieve our full potential. We are committed to transparency, collaboration, and accountability, and we believe that OKRs are an essential tool for empowering our team and achieving our mission </b>, and we believe that using OKRs is an essential tool to help us achieve our goals. <br/>
                 <br />
@@ -17,6 +17,9 @@ const AboutUs = () => {
                 Using OKRs has been a game-changer for our organization. By setting clear, measurable objectives and tracking our progress towards those objectives, we have been able to improve our performance, identify areas for growth, and achieve our goals faster than ever before. <br />
                 <br />
                 We are passionate about helping other organizations to achieve success with OKRs. Whether you're just getting started with OKRs or you're looking to take your implementation to the next level, we're here to help. Contact us today to learn more about our services and how we can help your organization achieve its goals. <br /> </p>
+                <br />
+        </div>
+
         </div>
     </div>
     
